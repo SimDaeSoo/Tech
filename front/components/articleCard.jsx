d42a85aeb56b05c9b7377370d2b9d8bc47839824
bigdeal <bigdeal@smartstudy.co.kr>
2020-04-28 18:33:30 +0900

    Card 수정.

--- front/components/articleCard.jsx
+++ front/components/articleCard.jsx
@@ -1,8 +1,13 @@
 import { Card } from 'antd';
-import { CalendarOutlined } from '@ant-design/icons';
+import { CalendarOutlined, UserOutlined, EyeOutlined } from '@ant-design/icons';
 import ArticleThumbnail from './articleThumbnail';
 
 export default class ArticleCard extends React.Component {
+    get created() {
+        const { article } = this.props;
+        return article.createdAt.slice(0, 10);
+    }
+
     render() {
         const { article, defaultImage } = this.props;
         return (
@@ -10,12 +15,18 @@ export default class ArticleCard extends React.Component {
                 className='article-card'
                 style={{ width: article.width, display: 'inline-block', margin: 10, boxShadow: '0px 6px 6px 0px rgba(0, 0, 0, 0.3)', textAlign: 'left' }}
                 cover={<ArticleThumbnail thumbnail={article.thumbnail ? article.thumbnail : defaultImage} />}
+                actions={[
+                    <div style={{ textAlign: 'center' }} key='createAt'>
+                        <CalendarOutlined />
+                        <em style={{ fontSize: '0.8em', marginLeft: '4px' }}> {this.created}</em>
+                    </div>,
+                    <div style={{ textAlign: 'center' }} key='author'>
+                        <UserOutlined />
+                        <em style={{ fontSize: '0.8em', marginLeft: '4px' }}>{article.user.username}</em>
+                    </div>
+                ]}
             >
                 <Card.Meta title={article.title} description={article.description} />
-                <div style={{ marginTop: '4px' }}>
-                    <CalendarOutlined />
-                    <em style={{ fontSize: '0.8em' }}> {article.createdAt}</em>
-                </div>
             </Card>
         );
     }
